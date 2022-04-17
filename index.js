189e09565067f685a1e8d3db9e402e45dde88afa
ReinaSparklechu <Sparklechus@gmail.com>
2022-04-18 01:18:26 +0800

    adding firebase to indexjs

--- index.js
+++ index.js
@@ -1,54 +1,24 @@
-
-var admin = require("firebase-admin");
-var serviceAccount = require('./reina-s-base-firebase-adminsdk-y5kk5-94f665d180.json');
-var request = require('request');
-var cheerio = require('cheerio');
-const { firestore } = require("firebase-admin");
+var { initializeApp } = require('firebase/app');
+var{ getFirestore, collection, getDocs } = require ('firebase/firestore/lite');
 //const functions = require('firebase-functions');
 
-admin.initializeApp({
-    credential:admin.credential.cert(serviceAccount)
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAAvLTRYevEOyle25R2Qg27D2WXWnSmaAE",
+    authDomain: "reina-s-base.firebaseapp.com",
+    projectId: "reina-s-base",
+    storageBucket: "reina-s-base.appspot.com",
+    messagingSenderId: "575805687872",
+    appId: "1:575805687872:web:c72bd7b5359f4113be77a5",
+    measurementId: "G-HQPZSLZ7T3"
+  };
+
+const app = initializeApp(firebaseConfig);
+var db = getFirestore(app);
+
+async function getList(db) {
+    const list = collection(db, 'Reddit');
+    listSnap = await getDocs(list);
+    const listed = listSnap.docs.map(doc => console.log(doc.data()));
 }
-)
-var db =admin.firestore();
-var today = Date();
-var allWords = [""];
-var filterWords = ['(v.redd.it)', '(i.redd.it)', 'the', 'then', 'this','much','was','were','this','that','they','them','their','his','her'
-,'not','for', 'The', 'are', 'you', 'don’t' , 'Not' , '[OC]','from', 'and', 'And', 'your','Your','can', 'who', 'Who', 'what', 'where', 'when', 'why', 'how' ];
-function scrapeReddit(){
-    request('https://old.reddit.com/r/popular/?geo_filter=GLOBAL', (error, response, html)=> {
-        if(!error && response.statusCode==200) {
-            const $ = cheerio.load(html);
-            $('p.title').each((i,title) =>
-            {
-                // set all to lower case for easy filtering
-                var headings = $(title).text().trim().toLowerCase();
-                var link = $(title.firstChild).attr("href");
-                console.log(headings);
-                let redditHeading = {
-                    header: headings,
-                    links : link,
-                    Time: today
-                }
-                db.collection('Reddit').add(redditHeading);
-                var words = headings.split(" ")
-                allWords = allWords.concat(words);
-            })
-            allWords = allWords.filter(word => word.length>2 && !filterWords.includes(word));
-            let collection = {
-                list: allWords
-            }
-            db.collection('reddit').add(collection);
-            console.log(allWords);
-        }
-    });
-}
-scrapeReddit();
-//var deletedata = db.collection('reddit').where('Date', '==', 'Thu Apr 14 2022 11:30:42 GMT+0800 (Singapore Standard Time)');
-//deletedata.get().then(function(querysnap){
-//    querysnap.forEach(function(doc) {
-//        doc.ref.delete();
-//    })
-//})
-//a.title may-blank loggedin
-//#thing_t3_u2ewn0 > div.entry.unvoted > div.top-matter > p.title > a
+ getList(db);
